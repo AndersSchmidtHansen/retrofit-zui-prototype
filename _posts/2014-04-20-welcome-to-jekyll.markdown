1e677e84748a93044018a7f3e1ff1bab66015cf4
Anders Schmidt Hansen <me@andersschmidt.com>
2016-02-09 07:28:08 +0100

    first commit

---
+++ _posts/2014-04-20-welcome-to-jekyll.markdown
@@ -0,0 +1,10 @@
+---
+layout: post
+title:  "Retrofitting ZUIs to Legacy Sites: An Impossible Task?"
+date:   2016-02-07 09:11:03
+categories: zui retrofitting
+---
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt quos dolorum eum similique deleniti vero, obcaecati facilis eveniet! At numquam molestias quibusdam, fugiat, consequatur veritatis consectetur ipsam earum ipsum. Earum.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem rem quae, obcaecati vel perferendis tenetur atque. Animi eaque sequi quisquam porro voluptatem distinctio quos quasi optio autem, iusto fugit expedita! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem rem quae, obcaecati vel perferendis tenetur atque. Animi eaque sequi quisquam porro voluptatem distinctio quos quasi optio autem, iusto fugit expedita!
---
+++ js/main.js
@@ -0,0 +1,145 @@
+$(function(){
+
+  $ZUIState = {
+    zoomed_in: false,
+    current_region_index: 0
+  }
+
+  $ZUIDocument = $(document)
+  $ZUIHtml = $('html')
+  $ZUIBody = $('body')
+  $ZUIRegion = $('.u-zui-region')
+  var $ZUIClose
+
+  
+  // Event Handlers
+  var $ZUIInitHandler = function(event) {
+    var $ZUIInitialBodyStyles = {
+      'transition-property': 'transform, transform-origin',
+      'transition-duration': '1s',
+      'transform-origin': '0px 0px',
+      'transform': 'scale(1)'
+    }
+    $ZUIRegion.each(function(){
+      $(this).css('transform', 'translateZ(0)')
+    })
+    $ZUIBody.css($ZUIInitialBodyStyles)
+
+
+    var $ZUICloseStyles = {
+      'top': '15px',
+      'right': '15px',
+      'width': '48px',
+      'height': '48px',
+      'border-radius': '8px',
+      'box-sizing': 'border-box',
+      'position': 'fixed',
+      'z-index': '1000',
+      'background-color': 'hsl(0, 0%, 86%)',
+      'box-shadow': '0 2px 2px 0 hsla(0,0%,0%,.22)',
+      'font-size': '48px',
+      'line-height': '1',
+      'text-align': 'center',
+      'cursor': 'pointer'           
+    }
+
+    $ZUIHtml.append('<div class="u-zui__close" style="display:none;">⇱</div>')
+
+    $ZUIClose = $('.u-zui__close')
+    $ZUIClose.css($ZUICloseStyles)
+    $ZUIClose.on('touchstart click', $ZUIZoomOutHandler)
+  }
+
+  var $ZUIZoomInHandler = function(event) {
+    var $ZUITaps = event.gesture.tapCount
+    var $ZUIDoubleTap = ( $ZUITaps == 2 ) ? true : false
+    var $ZUICurrentRegion = $(event.target)
+    var $ZUITargetXPosition = $ZUICurrentRegion.offset().left
+    var $ZUITargetYPosition = $ZUICurrentRegion.offset().top
+
+    var $ZUIZoomedInStyles = {
+      'transform': 'scale(2) translateZ(0)',
+      'transform-origin': $ZUITargetXPosition + 'px ' + $ZUITargetYPosition + 'px',
+      'overflow-x': 'hidden'
+    }
+
+    if ( $ZUIDoubleTap ) {
+      if ( !$ZUIState.zoomed_in ) { 
+        $ZUIBody.css($ZUIZoomedInStyles)
+        $ZUIState.zoomed_in = true
+        $ZUICurrentRegion.addClass('u-zui-region--in-focus')
+
+        $ZUIClose.css({
+          'top': ($ZUIHtml.offset().top + 15) + 'px',
+          'right': ($ZUIHtml.offset().left + 15) + 'px'
+        }).fadeIn()
+      }
+    }
+  }
+
+  var $ZUIZoomOutHandler = function(event) {
+    var $ZUIZoomedOutStyles = {
+      'transform': 'scale(1) translateZ(0)',
+      'transform-origin': '0px 0px'
+    }
+
+    if ( $ZUIState.zoomed_in ) {
+      $ZUIBody.css($ZUIZoomedOutStyles)
+      $ZUIState.zoomed_in = false
+      $('.u-zui-region--in-focus').removeClass('u-zui-region--in-focus')
+      $ZUIClose.fadeOut()    
+    }
+  }
+
+
+
+  var $ZUISwipeLeftHandler = function(event) {
+
+    var $ZUINextXPosition = $ZUIRegion.eq($ZUIState.current_region_index + 1).offset().left
+    var $ZUINextYPosition = $ZUIRegion.eq($ZUIState.current_region_index + 1).offset().top
+
+    var $ZUISwipeLeftStyles = {
+      'transform-origin': $ZUINextXPosition + 'px ' + $ZUINextYPosition + 'px'
+    }
+
+    if ( $ZUIState.zoomed_in ) {
+      $ZUIBody
+      .css($ZUISwipeLeftStyles)
+      .stop().animate({
+        scrollTop: 0,
+        scrollLeft: 0
+      }, 800)
+    }
+  }
+
+  var $ZUISwipeRightHandler = function(event) {
+    var $ZUIBackXPosition = $ZUIRegion.eq($ZUIState.current_region_index - 1).offset().left
+    var $ZUIBackYPosition = $ZUIRegion.eq($ZUIState.current_region_index - 1).offset().top
+
+    var $ZUISwipeRightStyles = {
+      'transform-origin': $ZUIBackXPosition + 'px ' + $ZUIBackYPosition + 'px'
+    }
+
+    if ( $ZUIState.zoomed_in ) {
+      $ZUIBody
+      .css($ZUISwipeRightStyles)
+      .stop().animate({
+        scrollTop: 0,
+        scrollLeft: 0
+      }, 800)
+    }
+  }  
+
+  // Initializing
+  $ZUIDocument.on('ready', $ZUIInitHandler)
+
+  // Handle Zooming In
+  $ZUIRegion
+  .hammer()
+  .on('tap', $ZUIZoomInHandler)
+  .on('swipeleft', $ZUISwipeLeftHandler)
+  .on('swiperight', $ZUISwipeRightHandler)
+
+
+
+})
